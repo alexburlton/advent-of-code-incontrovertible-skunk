ecbef9d28b198435cfcebe303bc3de7e2d66de15
alexburlton <alexburlton@hotmail.com>
2019-12-17 21:34:57 +0000

    Day 18 prep

---
+++ src/main/scala/Day18.scala
@@ -0,0 +1,5 @@
+class Day18 extends AbstractPuzzle(18) {
+  override def partA(): Any = -1
+
+  override def partB(): Any = -1
+}
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,6 @@
 
 object Main {
-  def days: List[AbstractPuzzle] = List(new Day17())
+  def days: List[AbstractPuzzle] = List(new Day18())
 
   def main(args: Array[String]): Unit = {
     days.foreach { _.run() }
